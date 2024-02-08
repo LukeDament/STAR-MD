3afb8da7a48fc11547e9e316b05aa187a2a3c664
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-02-08 14:56:06 +0100

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,15 +1,21 @@
-const {cmd } = require('../lib')
-cmd({
-        pattern: "insta",
-        desc: "download instagram post.",
-        category: "downloader",
-        filename: __filename
-    },
-    async(Void, citel,text,{isCreator}) => {
-        const { Insta } = require('../lib')
-if(!text) return citel.reply('Need post url.')
-let response = await Insta(text)
-for (let i=0;i<response.length;i++) {
-await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
-}
-    });
+ const {cmd , Config , prefix,getBuffer,tlang,fetchJson } = require('../lib')
+ const fetch = require('node-fetch')
+
+
+ let cap = `╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 ${Config.botname}`
+ cmd({
+         pattern: "insta",
+         alias: ["ig"],
+         desc: "download instagram videos",
+         category: "downloader",
+         use: "paste insta video link"
+     },
+     async(Void,citel,text) => {
+ if(!text) return citel.reply('Give me insta video link')
+ let txt = text ? text.split(" ")[0]:'';
+ if (!/instagram/.test(txt)) return await citel.reply(`Please give me valid instagram video link..!`);
+ let data;
+ try{ data= await (await fetch(`https://vihangayt.me/download/instagram2?url=${text}`)).json();} 
+ catch { return citel.reply(`An error occurred`);  }
+ return Void.sendMessage(citel.chat, {video : {url : data.result[0] },caption: cap,width: 600,height: 490, },{ quoted: citel })
+})
