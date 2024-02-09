4c7e04ca3e4170e3c7b6145e1c930ee0974b1af8
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-02-09 21:39:13 +0100

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -91,7 +91,7 @@ cmd({
         citel.reply(`*Check your Pm ${tlang().greet}*`);
         await Void.sendMessage(`${citel.sender}`, {
             image: log0,
-            caption: `*Group Name: Secktor-Support*\n*Group Link:* https://chat.whatsapp.com/Bl2F9UTVU4CBfZU6eVnrbC`,
+            caption: `*Group Name:⭐STAR⭐-Support*\n*Group Link:* https://chat.whatsapp.com/Lg0lY4M1k8oDMYzylg86xs`,
         });
 
     }
@@ -994,7 +994,7 @@ cmd({ on: "text" }, async(Void, citel) => {
 ║ *🛑Exp*: ${sck1.xp} / ${Levels.xpFor(sck1.level + 1)}
 ║ *📍Role*: *${role}*
 ║ *Enjoy🥳*
-╚════════════╝
+├┬┴┬┴ ☣️ՏTᗩᖇ-ᗰᗪ☣️ ┬┴┬┴┤
 `,
             }, {
                 quoted: citel,
